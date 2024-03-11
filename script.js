23cc794501ab789f03a74e92b27806f3ee0759bc
Dianne <daramirez1205@gmail.com>
2024-03-12 03:33:49 +0800

    added dom methods to alter battlegrounds whenever a button is clicked

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
-        // function that returns a random number 1-3
-        // Math.random returns a floating point between 0 and 1
-        // Multiplying this by n gives a floating point number between 0 (inclusive) and n (exclusive).
-        // (Math.random() * 3) => random number multiplied by the no. of outcomes (3)
-        // + 1 because 1 is the start number; just in case it gives 0
+    // function that returns a random number 1-3
+    // Math.random returns a floating point between 0 and 1
+    // Multiplying this by n gives a floating point number between 0 (inclusive) and n (exclusive).
+    // (Math.random() * 3) => random number multiplied by the no. of outcomes (3)
+    // + 1 because 1 is the start number; just in case it gives 0
 
     let randomNum = Math.floor((Math.random() * 3) + 1);
     
@@ -17,11 +17,6 @@ function getComputerChoice() {
         return "SCISSORS";
     }
 }
-
-let playerScore = 0;
-let computerScore = 0;
-
-
 
 function playRound(playerSelection, computerSelection) {
     // this function oversees the game
@@ -63,23 +58,78 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// ----- assigning buttons to playerSelection ------
+
+function alterComputerIcon(computerSelection) {
+    if (computerSelection === "ROCK") {
+        computerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #9D0191");
+        computerChoiceIcon.setAttribute("src", "icons/rock-icon.png");
+        computerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+    } else if (computerSelection === "PAPER") {
+        computerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #FAA300");
+        computerChoiceIcon.setAttribute("src", "icons/paper-icon.png");
+        computerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+    } else {
+        computerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #FF1E1E");
+        computerChoiceIcon.setAttribute("src", "icons/scissors-icon.png");
+        computerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+    }
+}
+
+
+// ------ adding query selectors for icons -------
+
+const playerChoiceBg = document.querySelector('.player-choice-f-icon');
+const playerChoiceIcon = document.querySelector('#player-icon');
+
+const computerChoiceBg = document.querySelector('.computer-choice-f-icon');
+const computerChoiceIcon = document.querySelector('#computer-icon');
+
+
+
+
+// ----- event listeners for buttons ------
 
 const rockAttack = document.querySelector('#rock-attack');
 rockAttack.addEventListener('click', () => {
-    playRound('ROCK', getComputerChoice());
+
+    let computerChoice = getComputerChoice()
+
+    playerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #9D0191");
+    playerChoiceIcon.setAttribute("src", "icons/rock-icon.png");
+    playerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+
+    alterComputerIcon(computerChoice);
+    playRound('ROCK', computerChoice);
 })
 
 const paperAttack = document.querySelector('#paper-attack');
 paperAttack.addEventListener('click', () => {
-    playRound('PAPER', getComputerChoice());
+    let computerChoice = getComputerChoice()
+
+    playerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #FAA300");
+    playerChoiceIcon.setAttribute("src", "icons/paper-icon.png");
+    playerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+
+    alterComputerIcon(computerChoice);
+    playRound('PAPER', computerChoice);
 })
 
 const scissorAttack = document.querySelector('#scissor-attack');
 scissorAttack.addEventListener('click', () => {
-    playRound('SCISSORS', getComputerChoice());
+
+    let computerChoice = getComputerChoice();
+
+    playerChoiceBg.setAttribute("style", "background-color: #ffffff; border: 20px solid #FF1E1E");
+    playerChoiceIcon.setAttribute("src", "icons/scissors-icon.png");
+    playerChoiceIcon.setAttribute("style", "height: 70%; width: 70%");
+
+    alterComputerIcon(computerChoice);
+    playRound('SCISSORS', computerChoice);
 })
 
+
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
     // play a first to 5 game using loops. keep score. winner/loser. player input through prompt.
